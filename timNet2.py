2813a1b77aee315afe98d883aa35c74a9ce987f4
hoyle.timothy@gmail.com <hoyle.timothy@gmail.com>
2023-10-26 07:30:22 -0700

    modularized some of the functions

--- timNet2.py
+++ timNet2.py
@@ -1,39 +1,43 @@
-#!/usr/bin/env python
+#!/usr/bin/python3.6
 
 from subprocess import Popen, PIPE
 
 #first, grab all of the interface names with 'tcpdump -D' cmd and place them
 #inside of an array
+i = 0
 
-intList = []
-n = 0
-p = Popen(["tcpdump","-D"], stdout=PIPE)
-while True:
-    line = p.stdout.readline()
-    if len(line.strip()) == 0: # line.strip() removes any leading or trailing
-                               # whitespace
-        break
-    else:
-        line = line.decode('utf-8')
-        line = line.split(' ')[0]
-        line = line.split('.')[1]
-        intList.append(line)
-        #print(intList[n])
-        n += 1
+def grab_interfaces():
+    intList = []
+    n = 0
+    p = Popen(["tcpdump","-D"], stdout=PIPE)
+    while True:
+        line = p.stdout.readline()
+        if len(line.strip()) == 0: # line.strip() removes any leading or trailing
+                                   # whitespace
+            break
+        else:
+            line = line.decode('utf-8')
+            line = line.split(' ')[0]
+            line = line.split('.')[1]
+            intList.append(line)
+            #print(intList[n])
+            n += 1
 
-    if not line:
-        break;
+        if not line:
+            break;
+    return intList
 
 #next, query the user for which interface they would like to listen on, and
 #list their possible options for them
 
-def choice_control():
-    range = 1
-    i = 0
-    print(f'Please make a choice from the following options: ')
-    for item in intList:
-        print(f'{range}. {intList[i]} interface')
-        range += 1
-        i += 1
-    choice = input('\nPlease input the number next to your choice: ').lower()
-choice_control()
+#def choice_control():
+#    range = 1
+#    i = 0
+#    print(f'Please make a choice from the following options: ')
+#    for item in intList:
+#        print(f'{range}. {intList[i]} interface')
+#        range += 1
+#        i += 1
+#    choice = input('\nPlease input the number next to your choice: ').lower()
+#choice_control()
+intList = grab_interfaces()
