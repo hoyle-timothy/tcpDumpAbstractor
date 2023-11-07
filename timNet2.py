cac2623511aa22f16334addb6219c511899d461e
Timothy Hoyle <hoyle.timothy@gmail.com>
2023-11-07 10:47:19 -0600

    Fleshed out first menu

--- timNet2.py
+++ timNet2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python
 
 from subprocess import Popen, PIPE
 
@@ -29,11 +29,9 @@ def grab_interfaces():
 #next, query the user for which interface they would like to listen on, and
 #list their possible options for them
 
-def choice_control():
-#    range = 1
-#    i = 0
+def choice_control(intList):
     n = 0
-    print(f"\nPLEASE MAKE A CHOICE FROM THE FOLLOWING OPTIONS: ")
+    print(f"\nHERE ARE YOU AVAILABLE INTERFACES: ")
     p = Popen(["tcpdump","-D"], stdout=PIPE)
     while True:
         line = p.stdout.readline()
@@ -42,16 +40,14 @@ def choice_control():
             break
         else:
             line = line.decode('utf-8')
-#            line = line.split(' ')[0]
-#            line = line.split('.')[1]
             print(line.strip()) #adding '.strip() removes any leading or
                                 #trailing whitespace
             n += 1
+    userChoice = input("PRESS THE NUMBER OF THE INTERFACE YOU WOULD LIKE TO LISTEN ON: ")
 #    for item in intList:
 #        print(f'{range}. {intList[i]} interface')
 #        range += 1
 #        i += 1
 #    choice = input('\nPlease input the number next to your choice: ').lower()
-choice_control()
 intList = grab_interfaces()
-print(intList[0])
+choice_control(intList)
